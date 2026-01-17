7defe7e4d4b06a601aa26abce5fb087363bb5c0f
PGKHS <pagnickhosafsy@outlook.com>
2026-01-17 11:07:32 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"log"
+	"os"
+	"todo/pkg/db"
 	"todo/pkg/server"
 )
 
 func main() {
+	dbFile := "scheduler.db"
+	if envDBFile := os.Getenv("TODO_DBFILE"); envDBFile != "" {
+		dbFile = envDBFile
+	}
+	if err := db.Init(dbFile); err != nil {
+		log.Fatal(err)
+	}
 	if err := server.Run("web"); err != nil {
 		log.Fatal(err)
 	}
